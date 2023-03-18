95f0ca63de8402057b8b2eab14287b9425ad4427
Bill Cordias <billcordias8@gmail.com>
2023-03-18 18:57:47 +0300

    commit

--- src/main/java/dev.diasco.movies/Movie.java
+++ src/main/java/dev.diasco.movies/Movie.java
@@ -1,20 +1,22 @@
 package dev.diasco.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
 import java.util.List;
 
 @Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
     private ObjectId id;
-
-    public ObjectId getId() {
-        return id;
-    }
-    public void setId(ObjectId id){
-        this.id = id;
-    }
-
     private String imdbId;
     private String title;
     private String releaseDate;
@@ -22,5 +24,6 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
-
+    @DocumentReference
+    private List<Review> reviewIds;
 }
