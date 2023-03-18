218a74a74c22b5e545b6c95766cea38a44e4348e
Bill Cordias <billcordias8@gmail.com>
2023-03-18 17:35:32 +0300

    commit

--- src/main/java/dev.diasco.movies/Movie.java
+++ src/main/java/dev.diasco.movies/Movie.java
@@ -7,6 +7,14 @@ import java.util.List;
 @Document(collection = "movies")
 public class Movie {
     private ObjectId id;
+
+    public ObjectId getId() {
+        return id;
+    }
+    public void setId(ObjectId id){
+        this.id = id;
+    }
+
     private String imdbId;
     private String title;
     private String releaseDate;
