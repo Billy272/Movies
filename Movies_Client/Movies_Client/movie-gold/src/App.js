c3aeab7dbbf647f987284a1266ea3bd830a0f145
Bill Cordias <billcordias8@gmail.com>
2023-04-07 23:11:01 +0300

    commit

---
+++ Movies_Client/Movies_Client/movie-gold/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+
+function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () =>{
+    try{
+      const response = await api.get('/movies');
+      console.log(response.data);
+      setMovies(response.data);
+    }
+    catch(err){
+      console.log(err); 
+    }
+    
+  }
+  useEffect(() =>{
+    getMovies();
+  },[])
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
