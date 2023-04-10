31eaa358d4c576ee28f325aae7c32618745160cf
Bill Cordias <billcordias8@gmail.com>
2023-04-10 13:05:01 +0300

    commit

--- Movies_Client/Movies_Client/movie-gold/src/App.js
+++ Movies_Client/Movies_Client/movie-gold/src/App.js
@@ -4,6 +4,8 @@ import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+
 
 function App() {
   const [movies, setMovies] = useState();
@@ -26,7 +28,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={Layout}>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}></Route>
           
         </Route>
       </Routes>
--- Movies_Client/Movies_Client/movie-gold/src/components/home/Home.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({movies}) => {
     return (
         <div>
            Welcome!!!
