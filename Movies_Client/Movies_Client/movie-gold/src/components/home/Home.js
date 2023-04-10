3d1e40ac4ca53acb3baf88b8e84c71f8196111d1
Bill Cordias <billcordias8@gmail.com>
2023-04-10 13:10:27 +0300

    commit

--- Movies_Client/Movies_Client/movie-gold/src/components/home/Home.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/home/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Hero from './Hero';
 
 const Home = ({movies}) => {
     return (
-        <div>
-           Welcome!!!
-        </div>
+        <Hero movies={movies} />
     );
 }
 
