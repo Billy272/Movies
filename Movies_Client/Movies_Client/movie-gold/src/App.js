c12a9c0b93b649ac1c813b2fbe264f66f76b2c07
Bill Cordias <billcordias8@gmail.com>
2023-04-10 14:01:26 +0300

    commit

--- Movies_Client/Movies_Client/movie-gold/src/App.js
+++ Movies_Client/Movies_Client/movie-gold/src/App.js
@@ -5,6 +5,7 @@ import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 
 function App() {
@@ -26,7 +27,9 @@ function App() {
   },[])
 
   return (
+    
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
