3e004d67209e605c07fd127ec7587e2e64ae3126
Bill Cordias <billcordias8@gmail.com>
2023-04-12 00:02:36 +0300

    commit

--- Movies_Client/Movies_Client/movie-gold/src/components/trailer/Trailer.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/trailer/Trailer.js
@@ -1,2 +1,12 @@
 import { useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import './Trailer.css';
+
+const Trailer = () => {
+    return(
+        <div className="trailer">
+
+        </div>
+    )
+}
+export default Trailer
