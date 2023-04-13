411e89ada313177627c28d80545a2b47bb203f6a
Bill Cordias <billcordias8@gmail.com>
2023-04-14 00:06:36 +0300

    Commit

--- Movies_Client/Movies_Client/movie-gold/src/components/trailer/Trailer.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/trailer/Trailer.js
@@ -9,7 +9,8 @@ const Trailer = () => {
 
     return(
         <div className="react-player-container">
-            {(key!=null)?<ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/`} />}
+            {(key!=null)?<ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/watch?v=${key}`} 
+            width='100%' height='100%' />:null}
         </div>
     )
 }
