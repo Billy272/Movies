2807c99091b34e5f37fd63bd1c629c5c23e57c27
Bill Cordias <billcordias8@gmail.com>
2023-04-16 00:03:37 +0300

    Commit

--- Movies_Client/Movies_Client/movie-gold/src/components/reviews/Reviews.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/reviews/Reviews.js
@@ -9,9 +9,61 @@ const Reviews = ({getMovieData,movie,reviews,setReviews}) =>{
     const revText = useRef();
     let params = useParams();
     const movieid = params.movieid;
-    return{
 
-    }
+    useEffect(() => {
+        getMovieData(movieid);
+    },[])
+
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Reviews</h3>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movie?.poster} alt="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                           <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review: " />
+                                </Col>
+                           </Row>
+                           <Row>
+                               <Col>
+                                   <hr />
+                               </Col>
+                           </Row>
+                        </>
+                    }
+                    {
+                        reviews?.map((review) => {
+                            return(
+                                <>
+                                    <Row>
+                                        <Col>{r.body}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <hr />
+                                        </Col>
+                                    </Row>
+                                </>
+                            )
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <hr />
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Reviews
