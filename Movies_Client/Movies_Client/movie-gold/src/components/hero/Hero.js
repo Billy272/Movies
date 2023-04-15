a2d8818ab7c52d2a331e0c00dec625ea5c84a279
Bill Cordias <billcordias8@gmail.com>
2023-04-15 23:20:15 +0300

    commit

--- Movies_Client/Movies_Client/movie-gold/src/components/hero/Hero.js
+++ Movies_Client/Movies_Client/movie-gold/src/components/hero/Hero.js
@@ -3,6 +3,7 @@ import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import {Paper} from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from "react-router-dom";
 
 const Hero = ({movies}) => {
     return(
@@ -22,6 +23,7 @@ const Hero = ({movies}) => {
                                                 <h4>{movie.title}</h4>
                                             </div>
                                             <div className="movie-buttons-container">
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}></Link>
                                                 <div className="play-button-icon-container">
                                                     <FontAwesomeIcon className="play-button-icon"
                                                     icon={faPlayCircle} />
